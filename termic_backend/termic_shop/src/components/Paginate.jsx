aae62bbc13d9fbbe7d660364d80f4698fed0a4b3
Harijohnson <harijn72@gmail.com>
2023-12-17 07:55:26 +0530

    new css changes

--- termic_backend/termic_shop/src/components/Paginate.jsx
+++ termic_backend/termic_shop/src/components/Paginate.jsx
@@ -31,7 +31,6 @@ function Paginate({pages, page, keyword='',isAdmin=false}) {
               },
             style: {
               backgroundColor: pageNumber === Number(page) ? 'gray' : 'white',
-              // Add other styles as needed
             },
           };
   
