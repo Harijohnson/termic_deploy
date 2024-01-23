09a412f14383c094d0668d8cc081bdd8b59a9ddf
Harijohnson <harijn72@gmail.com>
2024-01-23 19:03:22 +0530

    new changes on seller details

--- termic_backend/termic_frontend/termic_shop/src/screens/SellerScreen.jsx
+++ termic_backend/termic_frontend/termic_shop/src/screens/SellerScreen.jsx
@@ -1,39 +1,39 @@
-import React,{ useEffect,useState } from 'react'
-import { Link,redirect,useLocation,useNavigate } from 'react-router-dom'
-import { Form,Button,Col,Row } from 'react-bootstrap'
-import  Loader   from '../components/Loader'
-import  Message   from '../components/Message'
-import { useDispatch,useSelector } from 'react-redux'
-import { registerCompany,companyDetails } from '../actions/productActions'
-import  FormContainer   from '../components/FormContainer'
-function SellerScreen() {
+// import React,{ useEffect,useState } from 'react'
+// import { Link,redirect,useLocation,useNavigate } from 'react-router-dom'
+// import { Form,Button,Col,Row } from 'react-bootstrap'
+// import  Loader   from '../components/Loader'
+// import  Message   from '../components/Message'
+// import { useDispatch,useSelector } from 'react-redux'
+// import { registerCompany,companyDetails } from '../actions/productActions'
+// import  FormContainer   from '../components/FormContainer'
+// function SellerScreen() {
     
     
-    const dispatch = useDispatch()
+//     const dispatch = useDispatch()
 
-    const location = useLocation();
-    const navigate = useNavigate();
+//     const location = useLocation();
+//     const navigate = useNavigate();
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { error,loading,userInfo } = userLogin
-
-
+//     const userLogin = useSelector(state => state.userLogin)
+//     const { error,loading,userInfo } = userLogin
 
 
 
 
-    useEffect (() => {
-        if (!userInfo){
-            navigate('/login')
-        }else{
-            dispatch(companyDetails())
-        }
-    },[navigate,userInfo,dispatch])
+
+
+//     useEffect (() => {
+//         if (!userInfo){
+//             navigate('/login')
+//         }else{
+//             dispatch(companyDetails())
+//         }
+//     },[navigate,userInfo,dispatch])
 
      
 
-    const [companyName,setCompanyName] = useState('')
-    const [aboutCompany,setAboutCompany] = useState('')
+//     const [companyName,setCompanyName] = useState('')
+//     const [aboutCompany,setAboutCompany] = useState('')
 
     
 
@@ -41,86 +41,169 @@ function SellerScreen() {
 
 
 
-    const companyDetailsFromSelector = useSelector(state => state.companyDetails)
-    const {companyDetails:{ companyName:companyNameBackend,aboutCompanyBackend:abtCom}={}} = companyDetailsFromSelector
+//     const companyDetailsFromSelector = useSelector(state => state.companyDetails)
+//     const {companyDetails:{ companyName:companyNameBackend,aboutCompanyBackend:abtCom}={}} = companyDetailsFromSelector
 
 
 
-    const submitHandeler = ((e)=>
-    {
-        e.preventDefault()
-        dispatch(registerCompany(companyName,aboutCompany,))
-        const success = true 
+//     const submitHandeler = ((e)=>
+//     {
+//         e.preventDefault()
+//         dispatch(registerCompany(companyName,aboutCompany,))
+//         const success = true 
 
-        if (success){
-            navigate('seller/details')
-        }
+//         if (success){
+//             navigate('seller/details')
+//         }
 
-    })
-  return (
-    <>  
-         { !companyNameBackend ? (
-              <FormContainer>
-                <h1>Create Company</h1>
+//     })
+//   return (
+//     <>  
+//          { !companyNameBackend ? (
+//               <FormContainer>
+//                 <h1>Create Company</h1>
 
-                {error && <Message variant='danger'>{error}</Message>}
-                  {loading && <Loader />}
-                <Form onSubmit={ submitHandeler }>
-                  <Form.Group controlId='companyname'>
-                      <Form.Label>
-                          Company Name
-                      </Form.Label>
-                      <Form.Control 
-                      type='text'
-                      placeholder='Company Name'
-                      value={companyName}
-                      onChange={(e)=>setCompanyName(e.target.value)}>
+//                 {error && <Message variant='danger'>{error}</Message>}
+//                   {loading && <Loader />}
+//                 <Form onSubmit={ submitHandeler }>
+//                   <Form.Group controlId='companyname'>
+//                       <Form.Label>
+//                           Company Name
+//                       </Form.Label>
+//                       <Form.Control 
+//                       type='text'
+//                       placeholder='Company Name'
+//                       value={companyName}
+//                       onChange={(e)=>setCompanyName(e.target.value)}>
 
-                      </Form.Control>
-                  </Form.Group>
-
-
-                  <Form.Group controlId='aboutcompany'>
-                      <Form.Label>
-                          About Company
-                      </Form.Label>
-                      <Form.Control 
-                      type='textarea'
-                      placeholder='About Company'
-                      value={aboutCompany}
-                      onChange={(e)=>setAboutCompany(e.target.value)}>
-
-                      </Form.Control>
-                  </Form.Group>
-
-                  <Button 
-                  type='submit'
-                  variant='primary' >
-                      Submit
-                  </Button>
+//                       </Form.Control>
+//                   </Form.Group>
 
 
-                </Form>
+//                   <Form.Group controlId='aboutcompany'>
+//                       <Form.Label>
+//                           About Company
+//                       </Form.Label>
+//                       <Form.Control 
+//                       type='textarea'
+//                       placeholder='About Company'
+//                       value={aboutCompany}
+//                       onChange={(e)=>setAboutCompany(e.target.value)}>
 
-                {/* <Row className = 'py-3'>
-                  <Col>
-                  New Customer ? <Link to ={redirect ? `/register?redirect = ${redirect}` : '/register'}>
-                      Register
-                  </Link>
-                  </Col>
-                </Row> */}
-              </FormContainer>
-        )
-        :
-        (
-             <>
-            {navigate('/seller/details')}
-            </>
-             )}
-    </>
+//                       </Form.Control>
+//                   </Form.Group>
+
+//                   <Button 
+//                   type='submit'
+//                   variant='primary' >
+//                       Submit
+//                   </Button>
+
+
+//                 </Form>
+
+//                 {/* <Row className = 'py-3'>
+//                   <Col>
+//                   New Customer ? <Link to ={redirect ? `/register?redirect = ${redirect}` : '/register'}>
+//                       Register
+//                   </Link>
+//                   </Col>
+//                 </Row> */}
+//               </FormContainer>
+//         )
+//         :
+//         (
+//              <>
+//             {navigate('/seller/details')}
+//             </>
+//              )}
+//     </>
  
-    )
+//     )
+// }
+
+
+// export default SellerScreen
+
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useDispatch, useSelector } from 'react-redux';
+import { registerCompany, companyDetails } from '../actions/productActions';
+import FormContainer from '../components/FormContainer';
+
+function SellerScreen() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, loading, userInfo } = userLogin;
+
+  const companyDetailsFromSelector = useSelector((state) => state.companyDetails);
+  const { companyDetails: { companyName: companyNameBackend, aboutCompanyBackend: abtCom } = {} } = companyDetailsFromSelector;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/login');
+    } else {
+      dispatch(companyDetails());
+    }
+  }, [navigate, userInfo, dispatch]);
+
+  const [companyName, setCompanyName] = useState('');
+  const [aboutCompany, setAboutCompany] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(registerCompany(companyName, aboutCompany)).then((success) => {
+      if (success) {
+        navigate('/seller/details');
+      }
+    });
+  };
+
+  return (
+    <div>
+      <FormContainer>
+        <h1>Create Company</h1>
+
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='companyname'>
+            <Form.Label>Company Name</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Company Name'
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='aboutcompany'>
+            <Form.Label>About Company</Form.Label>
+            <Form.Control
+              type='textarea'
+              placeholder='About Company'
+              value={aboutCompany}
+              onChange={(e) => setAboutCompany(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type='submit' variant='primary'>
+            Submit
+          </Button>
+        </Form>
+      </FormContainer>
+      {companyNameBackend && (
+        <Message variant='info'>
+          Company already exists. You will be redirected to the seller details page.
+        </Message>
+      )}
+    </div>
+  );
 }
 
-
-export default SellerScreen
+export default SellerScreen;
